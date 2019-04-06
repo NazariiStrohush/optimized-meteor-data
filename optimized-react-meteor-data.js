85c93c0a266f32c8cc12a5982a803c0ce63ea9bb
NazariiStrohush <nazarii.strohush@gmail.com>
2019-04-06 17:02:32 +0300

    Fixed indentation

--- optimized-react-meteor-data.js
+++ optimized-react-meteor-data.js
@@ -11,51 +11,51 @@ import { Tracker } from 'meteor/tracker';
 const trueFn = () => true;
 
 export const pureWithTracker = function (...args) {
-	return function (WrappedComponent) {
-		return class PureWithTracker extends React.Component {
-			constructor(props) {
-				super(props);
-				this.state = {
-					trackerResult: {},
-				};
-				this.shouldRerunTracker = trueFn;
+  return function (WrappedComponent) {
+    return class PureWithTracker extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          trackerResult: {},
+        };
+        this.shouldRerunTracker = trueFn;
 
-				if (typeof args[0] === 'function') {
-					if (typeof args[1] === 'function') {
-						this.shouldRerunTracker = args[0];
-						this.trackerFn = args[1];
-					} else {
-						this.trackerFn = args[0];
-					}
-				}
-			}
+        if (typeof args[0] === 'function') {
+          if (typeof args[1] === 'function') {
+            this.shouldRerunTracker = args[0];
+            this.trackerFn = args[1];
+          } else {
+            this.trackerFn = args[0];
+          }
+        }
+      }
 
-			componentWillReceiveProps(nextProps) {
-				if (this.shouldRerunTracker(this.props, nextProps)) {
-					this.setState({
-						trackerResult: this.trackerFn(nextProps),
-					})
-				}
-			}
+      componentWillReceiveProps(nextProps) {
+        if (this.shouldRerunTracker(this.props, nextProps)) {
+          this.setState({
+            trackerResult: this.trackerFn(nextProps),
+          })
+        }
+      }
 
-			componentDidMount() {
-				this.trackerHandler = Tracker.autorun(() => {
-					this.setState({
-						trackerResult: this.trackerFn(this.props),
-					});
-				});
-			}
+      componentDidMount() {
+        this.trackerHandler = Tracker.autorun(() => {
+          this.setState({
+            trackerResult: this.trackerFn(this.props),
+          });
+        });
+      }
 
-			componentWillUnmount() {
-				if (this.trackerHandler) {
-					this.trackerHandler.stop();
-				}
-			}
+      componentWillUnmount() {
+        if (this.trackerHandler) {
+          this.trackerHandler.stop();
+        }
+      }
 
-			render() {
-				return <WrappedComponent {...this.props} {...this.state.trackerResult} />
-			}
-		};
-	}
+      render() {
+        return <WrappedComponent {...this.props} {...this.state.trackerResult} />
+      }
+    };
+  }
 };
 
--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
-	name: 'optimized-react-meteor-data',
-	version: '1.0.0',
-	// Brief, one-line summary of the package.
-	summary: 'HOC which using withTracker BUT provide ability to return previous result and prevent useless cascade calculations',
-	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/NazariiStrohush/optimized-react-meteor-data',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
-	documentation: 'README.md'
+  name: 'optimized-react-meteor-data',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'HOC which using withTracker BUT provide ability to return previous result and prevent useless cascade calculations',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/NazariiStrohush/optimized-react-meteor-data',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.3');
-	api.use('tracker');
-	api.use('ecmascript');
-	api.mainModule('optimized-react-meteor-data.js');
+  api.versionsFrom('1.3');
+  api.use('tracker');
+  api.use('ecmascript');
+  api.mainModule('optimized-react-meteor-data.js');
 });
 
 Npm.depends({
-	react: "16.0.0",
+  react: "16.0.0",
 });
