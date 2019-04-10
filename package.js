44c70dae3afa78b21f19e10f8fc6fbf7cc6e4edb
NazariiStrohush <nazarii.strohush@gmail.com>
2019-04-10 13:29:40 +0300

    Bump version according to changed

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nazariistrohush:pure-meteor-react',
-  version: '1.0.2',
+  version: '1.0.3',
   summary: 'Optimized react meteor data',
   git: 'https://github.com/NazariiStrohush/optimized-react-meteor-data',
   documentation: 'README.md'
